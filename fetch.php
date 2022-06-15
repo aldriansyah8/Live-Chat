879e59396ad34737be7c0cf5ab2eef8293abaad3
Leo Siribere <mysonborneo@gmail.com>
2022-06-15 20:54:40 +0700

    Add files via upload

---
+++ fetch.php
@@ -0,0 +1,12 @@
+<?php
+//fetch.php
+$connect = mysqli_connect("localhost", "root", "root", "ajax_test");
+$query = "SELECT * FROM tbl_tweet ORDER BY tweet_id DESC";
+$result = mysqli_query($connect, $query);
+if(mysqli_num_rows($result) > 0)
+{
+ while($row = mysqli_fetch_array($result))
+ {
+  echo '<p>'.$row["tweet"].'</p>';
+ }
+}
---
+++ insert.php
@@ -0,0 +1,9 @@
+<?php
+//insert.php
+if(isset($_POST["tweet"]))
+{
+ $connect = mysqli_connect("localhost", "root", "root", "ajax_test");
+ $tweet = mysqli_real_escape_string($connect, $_POST["tweet"]);
+ $sql = "INSERT INTO tbl_tweet (tweet) VALUES ('".$tweet."')";
+ mysqli_query($connect, $sql);
+}
